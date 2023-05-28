4c695e658270e45af65f7ff8dd577b5971bf4d16
workwithkp96 <134717527+workwithkp96@users.noreply.github.com>
2023-05-28 21:24:33 +0530

    Application file

---
+++ WeatherUappApplication.java
@@ -0,0 +1,13 @@
+package com.weatherapp.WeatherUapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherUappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeatherUappApplication.class, args);
+	}
+
+}
