55f0949086d280052e3c2d91bcc85e69d7f72b73
Karan Paul <134717527+workwithkp96@users.noreply.github.com>
2023-12-06 22:14:30 +0530

    Revert "updated the main function"

--- WeatherUappApplication.java
+++ WeatherUappApplication.java
@@ -7,9 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class WeatherUappApplication {
 
 	public static void main(String[] args) {
-
-		//SpringApplication.run(WeatherUappApplication.class, args);
-		System.out.println("this is it");
+		SpringApplication.run(WeatherUappApplication.class, args);
 	}
 
 }
